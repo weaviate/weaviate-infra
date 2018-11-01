4fd7ab33864e519c6041afceec953654b8501f7a
Etienne Dilocker <etienne@dilocker.de>
2018-11-01 20:00:36 +0100

    gh-11 generate exactly 1 property per thing with a primitive dataType

--- verification/index.js
+++ verification/index.js
@@ -14,12 +14,12 @@ const readWordsFromFile = () => fs
   .map(line => line.split(' ')[0]);
 
 const createThings = (amount, words) => uniqueNumbersBetween(amount, words.length - 1)
-  .map(wordIndex => thingClassFromName(words[wordIndex]));
+  .map(wordIndex => thingClassFromName(words[wordIndex], words));
 
 function main() {
   const words = readWordsFromFile();
   const things = createThings(numberOfThings, words);
-  console.log(things);
+  console.log(JSON.stringify(things, null, 2));
 }
 
 main();
--- verification/ontology.js
+++ verification/ontology.js
@@ -7,18 +7,40 @@ export type Property = {
 
 }
 
-export type Thing = {
+export type ThingClass = {
   class: string,
   description: string,
   properties: Array<Property>
 }
 
-function thingClassFromName(className: string): Thing {
+const allowedPrimitiveDataTypes = ['number', 'string', 'boolean'];
+
+function randomSingleItemFromWords(words: Array<string>) {
+  return words[Math.floor(Math.random() * (words.length - 1))];
+}
+
+function capitalizeWord(word: string) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+// randomProperty creates a property with one of the allowed primitive types
+// ignores references for now
+function randomProperty(words: Array<string>): Property {
   return {
-    class: className,
-    description: 'No description on this auto-generated thing',
-    properties: [],
+    name: randomSingleItemFromWords(words).toLowerCase(),
+    '@datatype': [randomSingleItemFromWords(allowedPrimitiveDataTypes)],
+    description: '',
+
   };
 }
+
+function thingClassFromName(className: string, words: Array<string>): ThingClass {
+  return {
+    class: capitalizeWord(className),
+    description: 'No description on this auto-generated thing',
+    properties: [randomProperty(words)],
+  };
+}
+
 
 module.exports = { thingClassFromName };
