fd960fb071a98c4e903b27f64b5370f780761583
Etienne Dilocker <etienne@dilocker.de>
2018-11-22 21:37:27 +0100

    gh-19 replace repition with higher-order fns in benchmark reporter

--- verification/src/benchmark.js
+++ verification/src/benchmark.js
@@ -46,118 +46,48 @@ class BenchmarkReporter {
 
   constructor(monitoring: Array<Monitoring>) {
     this.monitoring = monitoring;
-    // this.successRate = this.successRate.bind(this);
-    this.averageAll = this.averageAll.bind(this);
-    this.averageSuccessful = this.averageSuccessful.bind(this);
-    this.averageFailed = this.averageFailed.bind(this);
-    this.fastestAll = this.fastestAll.bind(this);
-    this.slowestSuccessful = this.slowestSuccessful.bind(this);
-    this.slowestFailed = this.slowestFailed.bind(this);
-    this.slowestAll = this.slowestAll.bind(this);
-    this.fastestSuccessful = this.fastestSuccessful.bind(this);
-    this.fastestFailed = this.fastestFailed.bind(this);
   }
 
-  filter(verb: string, resource: string): Array<Monitoring> {
-    return this.monitoring.filter(item => (
+  all = (verb: string, resource: string): Array<Monitoring> => this
+    .monitoring.filter(item => (
       item.verb === verb && item.resource === resource
-    ));
-  }
+    ))
 
-  averageAll(verb: string, resource: string): string {
-    const items = this.filter(verb, resource);
-    return formatMs(averageTime(items));
-  }
+  successful = (verb: string, resource: string): Array<Monitoring> => this
+    .all(verb, resource)
+    .filter(i => !!i.success)
 
-  averageSuccessful(verb: string, resource: string): string {
-    const items = this.filter(verb, resource)
-      .filter(i => !!i.success);
-    return formatMs(averageTime(items));
-  }
-
-  averageFailed(verb: string, resource: string): string {
-    const items = this.filter(verb, resource)
-      .filter(i => !i.success);
-    return formatMs(averageTime(items));
-  }
-
-  slowestAll(verb: string, resource: string): string {
-    const items = this.filter(verb, resource);
-    try {
-      return formatMs(slowestTime(items));
-    } catch (e) {
-      return 'n/a';
-    }
-  }
-
-  slowestSuccessful(verb: string, resource: string): string {
-    const items = this.filter(verb, resource)
-      .filter(i => !!i.success);
-    try {
-      return formatMs(slowestTime(items));
-    } catch (e) {
-      return 'n/a';
-    }
-  }
-
-  slowestFailed(verb: string, resource: string): string {
-    const items = this.filter(verb, resource)
-      .filter(i => !i.success);
-    try {
-      return formatMs(slowestTime(items));
-    } catch (e) {
-      return 'n/a';
-    }
-  }
-
-  fastestAll(verb: string, resource: string): string {
-    const items = this.filter(verb, resource);
-    try {
-      return formatMs(fastestTime(items));
-    } catch (e) {
-      return 'n/a';
-    }
-  }
-
-  fastestSuccessful(verb: string, resource: string): string {
-    const items = this.filter(verb, resource)
-      .filter(i => !!i.success);
-    try {
-      return formatMs(fastestTime(items));
-    } catch (e) {
-      return 'n/a';
-    }
-  }
-
-  fastestFailed(verb: string, resource: string): string {
-    const items = this.filter(verb, resource)
-      .filter(i => !i.success);
-    try {
-      return formatMs(fastestTime(items));
-    } catch (e) {
-      return 'n/a';
-    }
-  }
+  failed = (verb: string, resource: string): Array<Monitoring> => this
+    .all(verb, resource)
+    .filter(i => !i.success)
 
   successRate = (verb: string, resource: string): string => {
-    const all = this.filter(verb, resource);
-    const success = all.filter(i => !!i.success);
+    const all = this.all(verb, resource);
+    const success = this.successful(verb, resource);
 
     return `${(success.length / all.length * 100).toFixed(2)} %`;
+  }
+
+  timeSet = (filterFn: Function, calculaterFn: Function) => (verb: string, resource: string) => {
+    try {
+      return formatMs(calculaterFn(filterFn(verb, resource)));
+    } catch (e) {
+      return 'n/a';
+    }
   }
 
   tableRowGroup(verb: string, resource: string): Array<Array<string>> {
     return [
       tableRow(verb, resource, 'SUCCESS_RATE', this.successRate),
-      tableRow(verb, resource, 'AVERAGE_ALL', this.averageAll),
-      tableRow(verb, resource, 'AVERAGE_SUCCESS', this.averageSuccessful),
-      tableRow(verb, resource, 'AVERAGE_FAILURE', this.averageFailed),
-      tableRow(verb, resource, 'FASTEST_ALL', this.fastestAll),
-      tableRow(verb, resource, 'FASTEST_SUCCESS', this.fastestSuccessful),
-      tableRow(verb, resource, 'FASTEST_FAILURE', this.fastestFailed),
-      tableRow(verb, resource, 'SLOWEST_ALL', this.slowestAll),
-      tableRow(verb, resource, 'SLOWEST_SUCCESS', this.slowestSuccessful),
-      tableRow(verb, resource, 'SLOWEST_FAILURE', this.slowestFailed),
+      tableRow(verb, resource, 'AVERAGE_ALL', this.timeSet(this.all, averageTime)),
+      tableRow(verb, resource, 'AVERAGE_SUCCESS', this.timeSet(this.successful, averageTime)),
+      tableRow(verb, resource, 'AVERAGE_FAILURE', this.timeSet(this.failed, averageTime)),
+      tableRow(verb, resource, 'FASTEST_ALL', this.timeSet(this.all, fastestTime)),
+      tableRow(verb, resource, 'FASTEST_SUCCESS', this.timeSet(this.successful, fastestTime)),
+      tableRow(verb, resource, 'FASTEST_FAILURE', this.timeSet(this.failed, fastestTime)),
+      tableRow(verb, resource, 'SLOWEST_ALL', this.timeSet(this.all, slowestTime)),
+      tableRow(verb, resource, 'SLOWEST_SUCCESS', this.timeSet(this.successful, slowestTime)),
+      tableRow(verb, resource, 'SLOWEST_FAILURE', this.timeSet(this.failed, slowestTime)),
     ];
   }
 
