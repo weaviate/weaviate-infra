ead5ab286eebf31b85e783865ca91edcb0217e57
Ian Crosby <ian.daniel.crosby@gmail.com>
2018-12-19 12:46:23 -0500

    add weaviate deploy

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -47,14 +47,15 @@ resource "google_container_node_pool" "primary_pool" {
 }
 
 # The following outputs allow authentication and connectivity to the GKE Cluster.
-output "client_certificate" {
-  value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
-}
-
-output "client_key" {
-  value = "${google_container_cluster.primary.master_auth.0.client_key}"
-}
-
-output "cluster_ca_certificate" {
-  value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
-}
+# Useful for debugging, can be removed
+# output "client_certificate" {
+#   value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
+# }
+# 
+# output "client_key" {
+#   value = "${google_container_cluster.primary.master_auth.0.client_key}"
+# }
+# 
+# output "cluster_ca_certificate" {
+#   value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
+# }
